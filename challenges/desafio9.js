3f5efd1dadd1b64f0984cc18c0160a58addb1855
Joao Antonio <joaopsilva.dev@gmail.com>
2022-09-08 15:49:05 -0300

    feat: :construction: requisito 9 completo

---
+++ challenges/desafio9.js
@@ -0,0 +1,14 @@
+db.produtos.find(
+    { 
+        valoresNutricionais: {
+            $elemMatch: {
+                tipo: "calorias",
+                quantidade: { $lte: 500 },
+            },
+        },  
+    },
+    {
+        _id: 0,
+        nome: 1,
+    },
+);
